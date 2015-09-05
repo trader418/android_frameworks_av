a5c209aa0a11ad7755237b85566efecbe08bfd49
Janson Kang <temasek71@gmail.com>
2015-09-05 16:11:45 +0800

    Revert "libstagefright: Recover when we start to thrash the cache"
    This reverts commit 7d3099274f118a9cb8ad668c04544c482e1a7cb3.

--- media/libstagefright/NuCachedSource2.cpp
+++ media/libstagefright/NuCachedSource2.cpp
@@ -599,8 +599,7 @@ ssize_t NuCachedSource2::readInternal(off64_t offset, void *data, size_t size) {
     }
 
     if (offset < mCacheOffset
-            || (offset >= (off64_t)(mCacheOffset + mCache->totalSize())
-                && offset >= (off64_t)(mCacheOffset + mLowwaterThresholdBytes))) {
+            || offset >= (off64_t)(mCacheOffset + mCache->totalSize())) {
         static const off64_t kPadding = 256 * 1024;
 
         // In the presence of multiple decoded streams, once of them will
